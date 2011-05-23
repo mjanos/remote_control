481756d4f37a33260e5d76ac37058e311a816fe3
Michael Janos <spikeybadooks@gmail.com>
2011-05-22 22:35:09 -0400

    first commit

---
+++ remote.c
@@ -0,0 +1,23 @@
+#include "remote.h"
+
+
+int main(){
+int choice;
+printf("Choose a number 1-play/pause 2-forward 3-back 4-stop\n");
+scanf("%d", &choice);
+switch(choice){
+case 1:
+printf("play/pause\n");
+break;
+case 2:
+printf("forward\n");
+break;
+case 3:
+printf("back\n");
+break;
+case 4:
+printf("stop\n");
+break;
+}
+return 0;
+}
