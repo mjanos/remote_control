079e56475b421546b1ba46e66e9c5a4b22de86c4
hnasar <Hashem.nasarat@tufts.edu>
2011-05-22 23:30:14 -0400

    updated indentation

--- remote.c
+++ remote.c
@@ -2,22 +2,22 @@
 
 
 int main(){
-int choice;
-printf("Choose a number 1-play/pause 2-forward 3-back 4-stop\n");
-scanf("%d", &choice);
-switch(choice){
-case 1:
-printf("play/pause\n");
-break;
-case 2:
-printf("forward\n");
-break;
-case 3:
-printf("back\n");
-break;
-case 4:
-printf("stop\n");
-break;
-}
-return 0;
+  int choice;
+  printf("Choose a number 1-play/pause 2-forward 3-back 4-stop\n");
+  scanf("%d", &choice);
+  switch(choice){
+  case 1:
+    printf("play/pause\n");
+      break;
+  case 2:
+    printf("forward\n");
+    break;
+  case 3:
+    printf("back\n");
+    break;
+  case 4:
+    printf("stop\n");
+    break;
+  }
+  return 0;
 }
